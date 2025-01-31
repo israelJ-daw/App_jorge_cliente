7014018345feeb676e013d52d91818f94d0483f0
Israel Jimenez <israel.jimenez.jimenez.al@iespolignosur.org>
2025-01-31 12:15:13 +0100

    ok

--- alquiler/views.py
+++ alquiler/views.py
@@ -7,9 +7,8 @@ def index(request):
     return render(request, "index.html")
 
 
+
 def usuarios_lista_api(request):
-    # Obtenemos todos los usuarios
-    response = requests.get('http://0.0.0.0:8080/api/v1/usuarios')
-    # Trasformamos la respuesta en JSON
-    usuarios= response.json()
-    return render(request, 'Usuario/lista_api.html', {"usuarios":usuarios})
+    response = requests.get('https://IsraelJimenez.pythonanywhere.com/api/v1/usuarios')
+    usuarios = response.json()
+    return render(request, 'Usuario/lista_api.html', {"usuarios": usuarios})
