ee4615051337411000ee432fcd409c1199f65e57
Israel Jimenez <israel.jimenez.jimenez.al@iespolignosur.org>
2025-02-07 21:32:17 +0100

    avanzadas

--- alquiler/urls.py
+++ alquiler/urls.py
@@ -7,12 +7,14 @@ urlpatterns = [
     #cliente
  
     path('usuarios_lista_api/', views.usuarios_lista_api, name='usuarios_lista_api'),
-
     path('usuarios_lista_api2/', views.usuarios_lista_api2, name='usuarios_lista_api2'),
-
     path('categoria_lista_api/', views.categorias_lista_api, name='categorias_lista_api'),
-
-    path('propiedades_lista_api/', views.propiedades_lista_api, name='propiedades_lista_api')
+    path('propiedades_lista_api/', views.propiedades_lista_api, name='propiedades_lista_api'),
+    path('usuarios_busqueda_simple/', views.usuarios_busqueda_simple, name='usuarios_busqueda_simple'),
+    path('usuarios_busqueda_avanzada/', views.usuarios_busqueda_avanzada, name='usuarios_busqueda_avanzada'),
+    path('buscar/categorias/', views.categorias_busqueda_avanzada, name='categorias_busqueda_avanzada'),
+    path('buscar/propiedades/', views.propiedades_busqueda_avanzada, name='propiedades_busqueda_avanzada'),
+    path('buscar/servicios_extra/', views.servicios_extra_busqueda_avanzada, name='servicios_extra_busqueda_avanzada'),
 
 
 
--- alquiler/views.py
+++ alquiler/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.core import serializers
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import *
 
 def index(request):
     return render(request, "index.html")
@@ -28,4 +28,83 @@ def categorias_lista_api(request):
     response = requests.get('https://israeljimenez.pythonanywhere.com/api/v1/categorias/')
     categorias = response.json()
     return render(request, 'Categoria/categoria_lista_api.html', {"categorias": categorias})
-    
+
+def usuarios_busqueda_simple(request):
+    formulario = BusquedaUsuarisForm(request.GET)
+
+    if formulario.is_valid():
+        response = requests.get(
+            'http://0.0.0.0:8080/api/v1/usuarios/busqueda_simple/',  # Cambia esta URL si es necesario por la de pythonanywhere, yo lo hago en loocal pero seria ls siguiente: https://israeljimenez.pythonanywhere.com/api/v1/usuarios/busqueda_simple/ 
+            params={'textoBusqueda': formulario.cleaned_data.get("textoBusqueda")}
+        )
+
+        if response.status_code == 200:
+            usuarios = response.json()  
+
+            return render(request, 'Usuario/usuarios_busqueda_simple.html', {'usuarios_mostrar': usuarios})
+        else:
+            print("Error en la búsqueda:", response.status_code, response.text)
+
+    return render(request, 'Usuario/usuarios_busqueda_simple.html', {'formulario': formulario})
+
+
+def usuarios_busqueda_avanzada(request):
+    formulario = BusquedaAvanzadaUsuarioForm(request.GET)
+
+    if request.GET and formulario.is_valid():  
+        response = requests.get(
+            'http://0.0.0.0:8080/api/v1/usuarios/busqueda_avanzada/',  #cambiar ls url si es necesario por  https://israeljimenez.pythonanywhere.com/api/v1/usuarios/busqueda_avanzada/
+            params=formulario.cleaned_data
+        )
+
+        if response.status_code == 200:
+            usuarios = response.json()
+            return render(request, 'Usuario/usuarios_busqueda_avanzada_resultado.html', {'usuarios_mostrar': usuarios})
+
+    return render(request, 'Usuario/usuarios_busqueda_avanzada.html', {'formulario': formulario})
+def categorias_busqueda_avanzada(request):
+    formulario = BusquedaCategoriaForm(request.GET)
+
+    if request.GET and formulario.is_valid():
+        response = requests.get(
+            'http://0.0.0.0:8080/api/v1/categorias/busqueda_avanzada/',  
+            params=formulario.cleaned_data
+        )
+
+        if response.status_code == 200:
+            categorias = response.json()
+            return render(request, 'Categoria/categorias_busqueda_avanzada_resultado.html', {'categorias': categorias})
+
+    return render(request, 'Categoria/categorias_busqueda_avanzada.html', {'formulario': formulario})
+
+
+def propiedades_busqueda_avanzada(request):
+    formulario = BusquedaPropiedadForm(request.GET)
+
+    if request.GET and formulario.is_valid():
+        response = requests.get(
+            'http://0.0.0.0:8080/api/v1/propiedades/busqueda_avanzada/',  
+            params=formulario.cleaned_data
+        )
+
+        if response.status_code == 200:
+            propiedades = response.json()
+            return render(request, 'Propiedad/propiedades_busqueda_avanzada_resultado.html', {'propiedades': propiedades})
+
+    return render(request, 'Propiedad/propiedades_busqueda_avanzada.html', {'formulario': formulario})
+
+
+def servicios_extra_busqueda_avanzada(request):
+    formulario = BusquedaServicioExtraForm(request.GET)
+
+    if request.GET and formulario.is_valid():
+        response = requests.get(
+            'http://0.0.0.0:8080/api/v1/servicios_extra/busqueda_avanzada/',  
+            params=formulario.cleaned_data
+        )
+
+        if response.status_code == 200:
+            servicios_extra = response.json()
+            return render(request, 'Servicio_extra/servicios_extra_busqueda_avanzada_resultado.html', {'servicios_extra': servicios_extra})
+
+    return render(request, 'Servicio_extra/servicios_extra_busqueda_avanzada.html', {'formulario': formulario})
